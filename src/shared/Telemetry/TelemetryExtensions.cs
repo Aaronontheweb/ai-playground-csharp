c6e3b3a2a7659bd7df5f3cff8fce74c21a086731
Aaron Stannard <aaron@petabridge.com>
2025-04-08 17:03:21 -0500

    Got the "Hello World" sample to work (#3)
    * got "hello world" off the ground
    
    * added real readme

---
+++ src/shared/Telemetry/TelemetryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace AI.Telemetry;
+
+public static class TelemetryExtensions
+{
+    public static ILoggingBuilder ConfigureAiLogging(this ILoggingBuilder builder)
+    {
+        var resourceBuilder = ResourceBuilder.CreateDefault();
+        resourceBuilder
+            .AddEnvironmentVariableDetector()
+            .AddTelemetrySdk()
+            .AddServiceVersionDetector();
+
+        builder.AddOpenTelemetry(options => { options.SetResourceBuilder(resourceBuilder); });
+
+
+        return builder;
+    }
+    
+    public static IServiceCollection ConfigureAiTelemetry(this IServiceCollection services,
+        Action<MeterProviderBuilder>? additionalMeters = null,
+        Action<TracerProviderBuilder>? additionalTracers = null)
+    {
+        services
+            .AddOpenTelemetry()
+            .ConfigureResource(builder =>
+            {
+                builder
+                    .AddEnvironmentVariableDetector()
+                    .AddTelemetrySdk()
+                    .AddServiceVersionDetector();
+            })
+            .UseOtlpExporter()
+            .WithMetrics(c =>
+            {
+                c.AddRuntimeInstrumentation();
+
+                additionalMeters?.Invoke(c);
+            })
+            .WithTracing(c => { additionalTracers?.Invoke(c); });
+
+        return services;
+    }
+}
