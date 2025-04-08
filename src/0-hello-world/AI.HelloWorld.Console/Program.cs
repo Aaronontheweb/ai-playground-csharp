2ac5f8a2101213e76495cbfc786ab572ffc9ad92
Aaron Stannard <aaron@petabridge.com>
2025-04-08 16:43:52 -0500

    got "hello world" off the ground

--- src/0-hello-world/AI.HelloWorld.Console/Program.cs
+++ src/0-hello-world/AI.HelloWorld.Console/Program.cs
@@ -1,3 +1,30 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using AI.Telemetry;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using OllamaSharp;
 
-var hostBuilder = new HostBuilder();
+var builder = new HostApplicationBuilder();
+
+builder.Logging.ConfigureAiLogging();
+
+builder.AddOllamaApiClient("chat")
+    .AddChatClient()
+    .UseFunctionInvocation()
+    .UseOpenTelemetry()
+    .UseLogging();
+
+builder.Services.ConfigureAiTelemetry();
+
+
+var app = builder.Build();
+
+await app.StartAsync();
+
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+var resp = await chatClient.GetResponseAsync("How many feet are in a meter?", cancellationToken:cts.Token);
+    
+Console.WriteLine(resp);
+await app.WaitForShutdownAsync();
